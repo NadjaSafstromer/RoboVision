a7a5bead42b6f5f9ec5465385038ca799d6adae2
Jbnlund <Jbnlund@gmail.com>
2025-04-16 09:14:23 +0200

    Update ColourIdentify.cpp

--- ColourIdentify.cpp
+++ ColourIdentify.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+
 cv::Scalar bluemin = cv::Scalar(102.5, 153, 102);
 cv::Scalar bluemax = cv::Scalar(125, 255, 255);
 int main() {
@@ -20,8 +21,19 @@ int main() {
     
     std::vector < std::vector < cv::Point>> contours;
     cv::findContours(mask, contours, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
-    cv::drawContours(copy, contours, -1, cv::Scalar(0, 255, 0), 2);
-    cv::imshow("Image", mask);
+    //cv::drawContours(copy, contours, -1, cv::Scalar(0, 255, 0), 2);
+    
+
+    // Bounding BOX (Rectangle)
+    for (size_t i = 0; i < contours.size();++i) {
+        cv::Rect boundRect = cv::boundingRect(contours[i]);
+        if (boundRect.width > 5)
+        cv::rectangle(copy, boundRect.tl(), boundRect.br(), (0, 0, 0), 3);
+    }
+    
+    cv::imshow("Image", copy);
+
+
     cv::waitKey(0);
     return 0;
 }
